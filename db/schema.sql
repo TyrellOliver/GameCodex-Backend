92592327d4e8143ba538befa852f94959db11225
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-11-07 14:08:54 -0500

    updated the database

--- db/schema.sql
+++ db/schema.sql
@@ -6,33 +6,10 @@ CREATE DATABASE games_dev;
 CREATE TABLE games (
  id SERIAL PRIMARY KEY,
  name TEXT NOT NULL,
- "release year" INT NOT NULL,
+ release_year INT NOT NULL,
  genre TEXT,
- rating INT NOT NULL,
+ rating VARCHAR(2) NOT NULL,
  discontinued BOOL,
- "game studio" TEXT
+ game_studio TEXT,
+ system TEXT
 );
-
-ALTER TABLE games RENAME "game studio" TO platform;
-
-ALTER TABLE games DROP COLUMN platform;
-
-
-CREATE TABLE platforms (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE games_platforms (
-  game_id INTEGER NOT NULL REFERENCES games(id),
-  platform_id INTEGER NOT NULL REFERENCES platforms(id)
-);
-
-
-ALTER TABLE games DROP COLUMN "release year";
-
-ALTER TABLE games ADD COLUMN "release year" INT NOT NULL;
-
-ALTER TABLE games DROP COLUMN rating;
-
-ALTER TABLE games ADD COLUMN rating TEXT NOT NULL;
