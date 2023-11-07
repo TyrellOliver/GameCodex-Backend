0b4dec3b87be3979d00ffac523f82aa630dae581
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-11-07 01:04:14 -0500

    connected the server to the database

---
+++ db/dbConfig.js
@@ -0,0 +1,15 @@
+const pgp = require('pg-promise');
+require('dotenv').config();
+
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+};
+
+// Passing an object with the necessary information to connect the server to the database
+
+const db = pgp(cn);
+
+module.exports = db;
